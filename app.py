7e9626b79fcd6a9e4571c675f556bdb957dcd5d9
Navdha Agarwal <navdha16250@iiitd.ac.in>
2019-08-18 18:45:37 +0530

    basic login page

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "hello_world"
+def home():
+    return render_template("index.html")
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    error = None
+    if (request.method == 'POST'):
+        if (request.form['userid'] != '101'):
+            error = 'Invalid userID. Please try again.'
+        else:
+            return redirect(url_for('home'))
+
+    return render_template('login.html',error=error)
 
 if (__name__ == '__main__'):
-    app.run()
+    app.run(debug=True)
