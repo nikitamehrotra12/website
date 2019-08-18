cae793daf0ebe5dc623fc17df4179442014f93c5
Navdha Agarwal <navdha16250@iiitd.ac.in>
2019-08-18 23:38:36 +0530

    session management

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
 
 app = Flask(__name__)
 
+app.secret_key = "random"
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
 @app.route('/')
+@login_required
 def home():
     return render_template("index.html")
 
@@ -13,9 +28,16 @@ def login():
         if (request.form['userid'] != '101'):
             error = 'Invalid userID. Please try again.'
         else:
+            session['logged_in'] = True
             return redirect(url_for('home'))
 
     return render_template('login.html',error=error)
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('login'))
 
 if (__name__ == '__main__'):
     app.run(debug=True)
